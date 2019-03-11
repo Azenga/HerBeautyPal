b07c49a332cb16245ae11c88df42c9489a4dc923
Azenga Kevin <toomuch573@gmail.com>
2019-03-11 21:16:01 +0300

    Changed how the fragments should switch in the HomeActivity.java

--- app/src/main/java/com/example/mercie/example/HomeActivity.java
+++ app/src/main/java/com/example/mercie/example/HomeActivity.java
@@ -1,12 +1,10 @@
 package com.example.mercie.example;
 
 import android.content.Intent;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,54 +13,90 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private ViewPager fragHolder;
-    private MyFragStatePagerAdapter myFragStatePagerAdapter;
     private Toolbar toolbar;
+
+    //Firebase Stuff
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        fragHolder = (ViewPager) findViewById(R.id.frag_holder);
-        myFragStatePagerAdapter = new MyFragStatePagerAdapter(getSupportFragmentManager());
+//        Firebase Initializations
+        mAuth = FirebaseAuth.getInstance();
 
-        myFragStatePagerAdapter.addFragment(new HomePageFragment(), "Home Page");
-        myFragStatePagerAdapter.addFragment(new ChangeProfileFragment(), "Change Profile");
 
-        fragHolder.setAdapter(myFragStatePagerAdapter);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
     }
 
-/*
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        animationDrawable.start();
+    public void displayFrag(int id) {
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        Fragment frag = null;
+
+
+        switch (id) {
+            case R.id.nav_changeprofile:
+                getSupportActionBar().setTitle("Change Profile");
+                frag = new ChangeProfileFragment();
+                break;
+            case R.id.nav_homepage:
+                getSupportActionBar().setTitle("Home");
+                frag = new HomePageFragment();
+                break;
+            case R.id.nav_feedback:
+                getSupportActionBar().setTitle("Feedback");
+                frag = new FeedbackFragment();
+                break;
+            case R.id.nav_about:
+                getSupportActionBar().setTitle("About");
+                frag = new AboutFragment();
+                break;
+            case R.id.nav_history:
+                getSupportActionBar().setTitle("Change Profile");
+                frag = new HistoryFragment();
+                break;
+            case R.id.nav_checkDetails:
+                getSupportActionBar().setTitle("Reservation Details");
+                frag = new CheckReservationDetailsFragment();
+                break;
+            default:
+                logout();
+        }
+
+        if (frag != null) {
+            ft.replace(R.id.container, frag).commit();
+        }
+
     }
-*/
+
+    private void logout() {
+        mAuth.signOut();
+        startActivity(new Intent(this, SigninAsActivity.class));
+        finish();
+    }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -72,19 +106,14 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_rateUs) {
             return true;
         }
@@ -95,42 +124,22 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        displayFrag(item.getItemId());
 
-        if (id == R.id.nav_changeprofile) {
-            toolbar.setTitle("Profile Settings");
-            fragHolder.setCurrentItem(1);
-        } else if (id == R.id.nav_homepage) {
-            toolbar.setTitle("Beauty Pal");
-            fragHolder.setCurrentItem(0);
-        } else if (id == R.id.nav_feedback) {
-            toolbar.setTitle("Feedback");
-            fragHolder.setCurrentItem(2);
-        } else if (id == R.id.nav_about) {
-            toolbar.setTitle("About");
-            fragHolder.setCurrentItem(3);
-
-        } else if (id == R.id.nav_history) {
-            toolbar.setTitle("History");
-            fragHolder.setCurrentItem(4);
-
-        } else if (id == R.id.nav_checkDetails) {
-            toolbar.setTitle("Reservation Details");
-            fragHolder.setCurrentItem(5);
-
-        } else if (id == R.id.nav_logout) {
-            toolbar.setTitle("Log Out");
-            fragHolder.setCurrentItem(6);
-
-        } else if (id == R.id.nav_share) {
-            toolbar.setTitle("Share");
-            fragHolder.setCurrentItem(7);
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user == null) {
+            startActivity(new Intent(this, SigninAsActivity.class));
+            finish();
+        }
     }
 }
